30a71e7e9fd6b4f1d8e20ee47b8c62ab0828d9d2
Lucas Henrique Silva Gomes <lucas.gomes@jobconvo.com>
2021-04-03 00:36:48 -0300

    Updating

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const btn_calcular = document.getElementById("confirm-btn");
+
+
+function calculateIMC(){
+    const nome = document.getElementById('nome').value;
+    const peso = document.getElementById("peso").value;
+    const altura = document.getElementById("altura").value;
+
+}
+
+const result = document.getElementById("result-area");
+
